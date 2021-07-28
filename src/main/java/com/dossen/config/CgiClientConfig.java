7005c062ada9718a8222f167c2b96fdfb179c7d3
wanxingming <wanxingming@dossen.com>
2021-07-28 11:47:08 +0800

    修改配置

--- src/main/java/com/dossen/config/CgiClientConfig.java
+++ src/main/java/com/dossen/config/CgiClientConfig.java
@@ -20,9 +20,9 @@ public class CgiClientConfig {
 
     public static String CGI_HOST = "cgi.dossen.com";
 
-    public static String CGI_APP_ID = "224357176743211009";
+    public static String CGI_APP_ID = "1408003042392957004";
 
-    public static String CGI_APP_SECRET = "257e189b13c6386b263358c80d2417c2";
+    public static String CGI_APP_SECRET = "6b5478d1af9b3cbadc7bc9553ec3c85d";
 
 
 //    public static String CGI_HOST = "cgi.test.dossen.com";
--- src/main/java/com/dossen/util/TestUtil.java
+++ src/main/java/com/dossen/util/TestUtil.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @项目名称：dossen-api-service
@@ -39,11 +37,11 @@ public class TestUtil {
 
     public static String test10(){
         List<String> list = new ArrayList<>();
-        list.add("62382838942666858");
+        list.add("156453727430254592");
 
         for (String id : list) {
             Map request = new HashMap<String,Object>();
-            request.put("appID","224357176743211009");
+            request.put("appID","1408003042392957004");
             request.put("userAccountID",id);
             BaseRP<Object> baseRP = CgiHttpClient.send("/oneid/user/system/authorize", request, new TypeReference<BaseRP<Object>>(){});
             System.out.println("调用结果:"+JSON.toJSONString(baseRP));
@@ -52,15 +50,11 @@ public class TestUtil {
         return "调用结果:"+list.size();
     }
 
-    public static void main(String[] args) throws ParseException {
 
-            String str = "是否为主键列(1是、-1不是) ";
-            String regEx = "[\n1234567890`~!@#$%^&*(+-=|{}':;',\\[\\].<>/?~！@#￥%……&*()（）——+|{}【】‘；：”“’。， 、？]";
-            str.replaceAll("", "");
-            Pattern p = Pattern.compile(regEx);
-            Matcher m = p.matcher(str);
-            String newString = m.replaceAll("").trim();
-            System.out.println(newString);
+
+
+
+    public static void main(String[] args) throws ParseException {
 
     }
 
